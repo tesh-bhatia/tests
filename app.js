15c1fd341df846b74b1ba9c559b182ff35d5c353
tesh-bhatia <nbhatia03@gmail.com>
2018-01-18 17:22:45 -0500

    Trying walgreens API

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ console.log(navigator.geolocation.getCurrentPosition(
         return position.coords
     }
 ));
+
+var lat = '33.7763931';
+var long = '-84.38837740000001'
+
+var WGobj = {
+    "apiKey": "4n8VgBaIAcwfqcxWAQSreiniwZAGXltd",
+    "affId": "storesapi",
+    "lat": lat,
+    "lng": long,
+    "requestType": "locator",
+    "act": "fndStore",
+    "view": "fndStoreJSON",
+}
+
+function WGAPI(){
+    $.post('https://services.walgreens.com/api/stores/search', WGobj).done(function(response){
+        console.log(response);
+    })
+}
